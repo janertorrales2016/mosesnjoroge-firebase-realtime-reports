971c6acd530885b0c453300ff2ac19654a479241
mosesgichuki96@gmail.com <mosesgichuki96@gmail.com>
2020-07-17 15:00:34 +0300

    mosesnjoroge-firebase-realtime-reports

---
+++ app/src/main/java/com/example/twostepauthentication/Constants.java
@@ -0,0 +1,30 @@
+package com.example.twostepauthentication;
+
+public class Constants
+{
+    public String First_Name,Other_Name,Phone;
+
+    public String getFirst_Name() {
+        return First_Name;
+    }
+
+    public void setFirst_Name(String first_Name) {
+        First_Name = first_Name;
+    }
+
+    public String getOther_Name() {
+        return Other_Name;
+    }
+
+    public void setOther_Name(String other_Name) {
+        Other_Name = other_Name;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+}
